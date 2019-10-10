1072e16d03b52a5ea2a71c24d6abf59cbc3d94dd
viktoriia <beliaeva.vv@phystech.edu>
2019-10-10 20:57:09 +0300

    lab6

--- lab6/catchBall2.py
+++ lab6/catchBall2.py
@@ -32,25 +32,39 @@ def new_ball():
     x = ball["x"] = rnd(100, 700)
     y = ball["y"] = rnd(100, 500)
     r = ball["r"] = rnd(30, 50)
-    ball["dy"] = rnd(-10, 10)
-    ball["dx"] = rnd(-10, 10)
+    ball["dy"] = rnd(-20, 20)
+    ball["dx"] = rnd(-20, 20)
     ball["id"] = canv.create_oval(x - r, y - r, x + r, y + r, fill=choice(colors), width=0)
-    root.after(1000, new_ball)
+    root.after(2000, new_ball)
 
 
 def ball_move():
     global ball
     id = ball["id"]
+    ball['x'] += ball['dx']
+    ball['y'] += ball['dy']
     canv.move(id, ball["dx"], ball["dy"])
+    check(ball)
     root.after(delta_t, ball_move)
+
+
+def check(ball):
+    x = ball['x']
+    y = ball['y']
+    r = ball['r']
+    if x>800 - r or x<r :
+            ball['dx'] *=(-1)
+            ball['dx'] += rnd(-1, 2)
+    if y>600 - r or y<r:
+            ball['dy'] *=(-1)
+            ball['dy'] += rnd(-1, 2)
 
 
 def click(event):
     coords = canv.coords(ball['id'])
-    print(coords,event)
-    r=ball["r"]
-    x = coords[0]+r
-    y = coords[1]+r
+    r = ball["r"]
+    x = coords[0] + r
+    y = coords[1] + r
     if ((event.x - x) ** 2 + (event.y - y) ** 2) ** 0.5 < r:
         add_point()
 
